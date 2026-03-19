23087ad6b76b072c8a0de5c7f1e7dc0a9222d9d5
Artem Negri <109627141+artemn@users.noreply.github.com>
2026-03-19 17:37:47 +0100

    HSAAS-2932 Get rid of abandoned composer packages (#7) (#8)
    Co-authored-by: Oleg Bannyi <oleg.bannyi@xmlshop.com>
    Co-authored-by: Oleg Bannyi <oleg.bannyi@gmail.com>

--- src/Services/DetectOverLimits/Notifier.php
+++ src/Services/DetectOverLimits/Notifier.php
@@ -3,32 +3,20 @@
 namespace xmlshop\QueueMonitor\Services\DetectOverLimits;
 
 use Illuminate\Support\Facades\App;
-use Pressutto\LaravelSlack\Slack;
+use xmlshop\QueueMonitor\Services\Slack\Slack;
 use xmlshop\QueueMonitor\Services\System\SystemResourceInterface;
 
 class Notifier
 {
-    /**
-     * @var ?Slack
-     */
-    private ?Slack $slack = null;
-
     public function __construct(private SystemResourceInterface $systemResource)
     {
-    }
-
-    private function getChannel(): Slack
-    {
-        if (null === $this->slack) {
-            $this->slack = app(Slack::class);
-        }
-        return $this->slack->to(config('monitor.alarm.recipient'));
     }
 
     public function send(string $message): void
     {
         if (!App::environment('local')) {
-            $this->getChannel()->send('*[GMT ' . now()->format('H:i') . ']*' . "\n" . $message);
+            Slack::to(config('monitor.alarm.recipient'))
+                ->send('*[GMT ' . now()->format('H:i') . ']*' . "\n" . $message);
         }
 
         if (!$this->systemResource->isParentProcessScheduler()) {
