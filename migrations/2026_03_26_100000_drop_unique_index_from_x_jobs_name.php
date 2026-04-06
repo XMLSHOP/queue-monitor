d24bdce644328492659e6a4e5509112181e09d20
Dan Petrov <102982034+petrov-xmlshop@users.noreply.github.com>
2026-04-06 13:21:39 +0300

    HSAAS-2960 Remove unique index by name from x_jobs  (#9)

---
+++ migrations/2026_03_26_100000_drop_unique_index_from_x_jobs_name.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropUniqueIndexFromXJobsName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::connection(config('monitor.db.connection'))
+            ->table('x_jobs', function (Blueprint $table) {
+                $table->dropUnique('x_jobs_name_unique');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::connection(config('monitor.db.connection'))
+            ->table('x_jobs', function (Blueprint $table) {
+                $table->unique('name');
+            });
+    }
+}
